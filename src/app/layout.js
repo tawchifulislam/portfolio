08adf9a99d8c0f2c2fe4263921f565d015fd2cd5
tawchifulislam <tawchif04@gmail.com>
2026-06-15 13:52:14 +0600

    Add id to

--- src/app/layout.js
+++ src/app/layout.js
@@ -107,6 +107,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col bg-[#0a0a0f] text-white">
         <script
+          id="google-schema-markup"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
